ab77078207e58bb102efbdd49b0c9e38edab4daf
albertogonper <alberto.gonzalez95@gmail.com>
2016-05-23 09:46:02 +0200

    Now the robot detects the room

--- robot_utils/RobotMain.py
+++ robot_utils/RobotMain.py
@@ -6,11 +6,13 @@ from robot_utils.Robot2 import *
 from robot_utils.LineDetectorPublisher import LineDetectorPublisher
 from robot_utils.RoomController import *
 from robot_utils.WallAvoider import WallAvoider
+from tools import ClassifierSVMLoader
 
 __author__ = 'Alberto'
 
 TIME_BETWEEN_NOTIFICATIONS = 2.0
 BEHAVIOR = "Simple"
+
 
 class RobotStatus:
     START_ROOM = "START_ROOM"
@@ -28,10 +30,11 @@ class RobotStatus:
 
 
 class RobotMain:
-    def __init__(self, robot, wall_avoider, room_controller):
+    def __init__(self, robot, wall_avoider, room_controller, room_detector):
         self._robot = robot
         self._wall_avoider = wall_avoider
         self._room_controller = room_controller
+        self._room_detector = room_detector
         self._status = RobotStatus.START_ROOM
         self._publisher = None
         self._last_notified = time.time()
@@ -60,6 +63,11 @@ class RobotMain:
         if now - self._last_notified > TIME_BETWEEN_NOTIFICATIONS:
             self._last_notified = now
             self._status = RobotStatus.next_status(self._status)
+            if self._status == RobotStatus.PASSAGE:
+                (lv, rv) = self._robot.distance()
+                cv = self._robot.ultrasound()
+                room = self._room_detector.predict((lv, rv, cv))
+                print("I'm leaving room: {}".format(room+1))
             print("Nuevo estado {}".format(self._status))
 
     def set_publisher(self, publisher):
@@ -70,6 +78,7 @@ class RobotMain:
             self._wall_avoider.move_without_crashing()
             time.sleep(0.1)
 
+
 def main():
     agent_robot = Robot(ULTRASONIC_CONFIG)
     kill_signal = threading.Event()
@@ -78,7 +87,7 @@ def main():
     room_controller = RoomController(agent_robot, camera=camera, stream=PiRGBArray(camera))
     wall_avoider = WallAvoider(agent_robot, kill_signal=kill_signal)
     light_detector = LineDetectorPublisher(agent_robot, kill_signal=kill_signal)
-    light_detector.daemon = True
+    room_detector = ClassifierSVMLoader('../results/svm-params-1462823669_34')
 
     try:
         brain_robot = RobotMain(robot=agent_robot, wall_avoider=wall_avoider, room_controller=room_controller)
@@ -92,6 +101,7 @@ def main():
         agent_robot.terminate()
         camera.close()
         print("END")
+
 
 if __name__ == '__main__':
     main()
